20f4517f93a48a938f4b8cb8c1a0dd9647249be9
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2023-04-10 17:38:22 +0200

    :alembic: Operator testing

--- src/test/java/be/dog/d/steven/webflux/OperatorTests.java
+++ src/test/java/be/dog/d/steven/webflux/OperatorTests.java
@@ -2,6 +2,9 @@ package be.dog.d.steven.webflux;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 public class OperatorTests {
     @Test
@@ -13,6 +16,45 @@ public class OperatorTests {
 
     @Test
     void second() {
+        Flux.range(1, 5)
+                .flatMap(i -> Flux.range(1, i))
+                .subscribe(System.out::println);
+    }
 
+    @Test
+    void third() {
+        Mono.just(3)
+                .flatMapMany(i -> Flux.range(1, i))
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    void fourth() throws InterruptedException {
+        Flux<Integer> oneToFive = Flux.range(1, 5)
+                .delayElements(Duration.ofMillis(200));
+        Flux<Integer> sixToTen = Flux.range(6, 5)
+                .delayElements(Duration.ofMillis(400));
+        Flux.concat(oneToFive, sixToTen).subscribe(System.out::println);
+        Thread.sleep(4000);
+    }
+
+    @Test
+    void fifth() throws InterruptedException {
+        Flux<Integer> oneToFive = Flux.range(1, 5)
+                .delayElements(Duration.ofMillis(200));
+        Flux<Integer> sixToTen = Flux.range(6, 5)
+                .delayElements(Duration.ofMillis(400));
+        Flux.merge(oneToFive, sixToTen).subscribe(System.out::println);
+        Thread.sleep(4000);
+    }
+
+    @Test
+    void sixth() throws InterruptedException {
+        Flux<Integer> oneToFive = Flux.range(1, 5)
+                .delayElements(Duration.ofMillis(200));
+        Flux<Integer> sixToTen = Flux.range(6, 5)
+                .delayElements(Duration.ofMillis(400));
+        Flux.zip(oneToFive, sixToTen, (n, m) -> n + ", " + m).subscribe(System.out::println);
+        Thread.sleep(4000);
     }
 }
